060f6d01728e56e4103f714ce76bac9680853955
adeeltahirdev <adeeltahir0301@gmail.com>
2026-04-05 19:43:05 +0500

    Made templates and flask skeleton

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-def main():
-    print("Hello from repominer!")
+from flask import Flask, render_template, request, redirect, send_file
+import requests
+import zipfile
+import io
 
 
-if __name__ == "__main__":
-    main()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/repo-view')
+def repo_view():
+    
+    if request.method == 'POST':
+        repo_url = request.form.get('repo_url')
+        
+        parts = repo_url.strip('/').split('/')
+        owner = parts[-2]
+        repo = parts[-1]
+        
+        url = f"https://api.github.com/repos/{owner}/{repo}/contents/"
+        response = requests.get(url)
+        contents = response.json()
+        
+        return render_template('repo.html', contents=contents, owner=owner, repo=repo)
+    
+    return redirect('/')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
